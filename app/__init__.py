e8cebed7f70d9e7edd372f0017d71b7ec98ffe7f
aprilx2222 <aprilxu2222@gmail.com>
2015-02-28 17:31:09 -0600

    df

--- app/__init__.py
+++ app/__init__.py
@@ -16,4 +16,24 @@ def addtask():
 def today():
     return render_template('today.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
 from app import views
